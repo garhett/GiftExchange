b7ad39b13b3fbd83c723ebd229cd8130b965a610
garhett <60421166+garhett@users.noreply.github.com>
2020-12-09 20:57:29 -0700

    Setup javadocs for GiftExchangeBuilder.java.

--- src/com/giftexchange/GiftExchangeBuilder.java
+++ src/com/giftexchange/GiftExchangeBuilder.java
@@ -2,12 +2,28 @@ package com.giftexchange;
 
 import java.util.ArrayList;
 
+/**
+ * Name: Garhett Anderson & Alissa Corona. Course: CIT-260. File(s): GiftExchangeBuilder.java,
+ * Child.java, Adult.java, Person.java. Date: 12/9/2020. About: This program enables the user to
+ * input each of their family members to get a back a randomized report of a gift exchange among
+ * each of the members of the list to then pass out to all family members prior to having the
+ * Christmas party. This program also saves the list of family members so it can be used again in
+ * the future when the program is reopened. Status: IN-PROGRESS.
+ */
 public class GiftExchangeBuilder {
 
+  /**
+   * Main method, driver method.
+   *
+   * @param args
+   */
   public static void main(String[] args) {
+    // Create each of the local variables for the person's characteristics.
     String name = "";
     String gender = "";
     int age = 0;
+
+    // Create each local list to be used and assigned for various uses.
     ArrayList<String> dislikedPeople = new ArrayList<>();
     ArrayList<String> likedItems = new ArrayList<>();
     ArrayList<Person> personList = new ArrayList<>();
