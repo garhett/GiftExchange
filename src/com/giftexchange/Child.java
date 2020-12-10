70abcda198d92e2f2addace222619fc205e4b70e
alissacorona <cor17003@byui.edu>
2020-12-09 20:26:26 -0700

    Hello Back!

--- src/com/giftexchange/Child.java
+++ src/com/giftexchange/Child.java
@@ -1,5 +1,28 @@
 package com.giftexchange;
 
-public class Child {
+import java.util.ArrayList;
 
+public class Child extends Person {
+ArrayList<String> itemsTheyLike = new ArrayList<>();
+
+public Child () {
+    super();
+    itemsTheyLike.set(0, "");
+}
+ public Child (String name, String gender, int age, ArrayList<String> itemsTheyLike) {
+    super (name, gender, age);
+    this.itemsTheyLike = itemsTheyLike;
+
+ }
+ public String toString(){
+    return "";
+
+ }
+    public ArrayList<String> getItemsTheyLike() {
+        return itemsTheyLike;
+    }
+
+    public void setItemsTheyLike(ArrayList<String> itemsTheyLike) {
+        this.itemsTheyLike = itemsTheyLike;
+    }
 }
