81120753c0bab86767c4bf7f6177dc740ae63318
garhett <60421166+garhett@users.noreply.github.com>
2020-12-09 20:12:12 -0700

    *Finished the Adult.java class.

--- src/com/giftexchange/Adult.java
+++ src/com/giftexchange/Adult.java
@@ -1,5 +1,28 @@
 package com.giftexchange;
 
-public class Adult {
+import java.util.ArrayList;
 
+public class Adult extends Person {
+  ArrayList<String> peopleTheyDislike = new ArrayList<>();
+
+  public Adult() {
+    peopleTheyDislike.set(0, "");
+  }
+
+  public Adult(String name, String gender, int age, ArrayList<String> peopleTheyDislike) {
+    super(name, gender, age);
+    this.peopleTheyDislike = peopleTheyDislike;
+  }
+
+  public String toString() {
+    return "";
+  }
+
+  public ArrayList<String> getPeopleTheyDislike() {
+    return peopleTheyDislike;
+  }
+
+  public void setPeopleTheyDislike(ArrayList<String> peopleTheyDislike) {
+    this.peopleTheyDislike = peopleTheyDislike;
+  }
 }
--- src/com/giftexchange/Person.java
+++ src/com/giftexchange/Person.java
@@ -1,5 +1,43 @@
 package com.giftexchange;
 
 public class Person {
+  private String name = "";
+  private String gender = "";
+  private int age = 0;
 
+  public Person() {}
+
+  public Person(String name, String gender, int age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+  }
+
+  public String toString() {
+    return "";
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
 }
