c08c0ae618cb427f146adbffa4008962a28d187b
alissacorona <cor17003@byui.edu>
2020-12-14 18:49:02 -0700

    Hello Back!

--- src/com/giftexchange/GiftExchangeBuilder.java
+++ src/com/giftexchange/GiftExchangeBuilder.java
@@ -1,6 +1,7 @@
 package com.giftexchange;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -27,22 +28,40 @@ public class GiftExchangeBuilder {
             "The user will have the ability to generate a random report if they need to create a new gift exchange list for this year");
     //get user input for adult list. Name, gender, and age. Also who they want to black list,
     //If they do not want to blacklist anyone have them enter 'none'.
+System.out.println("Please enter adult name, gender, and age you want to include in the gift exchange. " +
+                  "Also add one name they want to blacklist, enter 'none' if not want to blacklist a name.");
 
     //get user input for child list. Name, gender, and age. Also what they like.
     //if they do not have a preference they can enter 'none'.
+
+    System.out.println("Please enter child name, gender, and age you want to include in the gift exchange." +
+                      "Also add one item they like, enter 'none' if they are not picky.");
+
+    //catch exceptions if user enters a letter instead of and number for age.
+
 
     //menu style: user enter 1 to show adult list
     //            user enter 2 to edit adult list. Add more people.
     //            user enter 3 to show child list
     //            user enter 4 to edit child list Add more people.
+    //            user enter 5 if finished with list.
 
     System.out.println("Enter 1 to show adult list. \n" +
                        "Enter 2 to edit adult list, add more people. \n" +
                        "Enter 3 to show child list. \n" +
                        "Enter 4 to edit child list, add more people. \n" +
-                       "Enter 5 if finished with listsL");
+                       "Enter 5 if finished with list");
 
 
+      // Generate random list for adult
+    Random generate = new Random();
+    String[] Adult= {};
+    System.out.println("Adult List:  " +  Adult;[generate.nextInt()];
+
+      // Generate random list for child
+    Random generate = new Random();
+    String[] Child = {};
+    System.out.println("Child List:  " +  Child;[generate.nextInt()];
 
     // Create each of the local variables for the person's characteristics. - Written by Garhett
 
