6033636caa74cdd6db0f0210c6817cac0fc861f1
garhett <60421166+garhett@users.noreply.github.com>
2020-12-14 20:48:47 -0700

    *Added functionality to list builder, and fixed scanner buffer issue.

--- src/com/giftexchange/GiftExchangeBuilder.java
+++ src/com/giftexchange/GiftExchangeBuilder.java
@@ -20,58 +20,76 @@ public class GiftExchangeBuilder {
    * @param args
    */
   public static void main(String[] args) {
-    // import scanner and list for Person
-    Scanner keyboard = new Scanner(System.in);
-    ArrayList<Person> g = new ArrayList<>();
-    //tell the user what the program does
-    System.out.println("Family gift exchanges are a holiday tradition in many families.\n" +
-            "The user will have the ability to generate a random report if they need to create a new gift exchange list for this year");
-    //get user input for adult list. Name, gender, and age. Also who they want to black list,
-    //If they do not want to blacklist anyone have them enter 'none'.
-System.out.println("Please enter adult name, gender, and age you want to include in the gift exchange. " +
-                  "Also add one name they want to blacklist, enter 'none' if not want to blacklist a name.");
+    //        // import scanner and list for Person
+    //        Scanner keyboard = new Scanner(System.in);
+    //        ArrayList<Person> g = new ArrayList<>();
+//            //get user input for adult list. Name, gender, and age. Also who they want to black
+//     list,
+//            //If they do not want to blacklist anyone have them enter 'none'.
+//         System.out.println("Please enter adult name, gender, and age you want to include in the
+//     gift
+//         exchange. " +
+//                          "Also add one name they want to blacklist, enter 'none' if not want to
+//         blacklist a name.");
+//
+//            //get user input for child list. Name, gender, and age. Also what they like.
+//            //if they do not have a preference they can enter 'none'.
+//
+//            System.out.println("Please enter child name, gender, and age you want to include in
+//     the
+//         gift exchange." +
+//                              "Also add one item they like, enter 'none' if they are not picky.");
+//
+//            catch exceptions if user enters a letter instead of and number for age.
+    //          // Generate random list for adult
+    //        Random generate = new Random();
+    //        String[] Adult= {};
+    //        System.out.println("Adult List:  " +  Adult;[generate.nextInt()];
+    //
+    //          // Generate random list for child
+    //        Random generate = new Random();
+    //        String[] Child = {};
+    //        System.out.println("Child List:  " +  Child;[generate.nextInt()];
 
-    //get user input for child list. Name, gender, and age. Also what they like.
-    //if they do not have a preference they can enter 'none'.
+    /* ALISSA CODE ABOVE. GARHETT CODE BELOW */
 
-    System.out.println("Please enter child name, gender, and age you want to include in the gift exchange." +
-                      "Also add one item they like, enter 'none' if they are not picky.");
-
-    //catch exceptions if user enters a letter instead of and number for age.
-
-
-    //menu style: user enter 1 to show adult list
-    //            user enter 2 to edit adult list. Add more people.
-    //            user enter 3 to show child list
-    //            user enter 4 to edit child list Add more people.
-    //            user enter 5 if finished with list.
-
-    System.out.println("Enter 1 to show adult list. \n" +
-                       "Enter 2 to edit adult list, add more people. \n" +
-                       "Enter 3 to show child list. \n" +
-                       "Enter 4 to edit child list, add more people. \n" +
-                       "Enter 5 if finished with list");
-
-        System.out.println("hello ");
-      // Generate random list for adult
-    Random generate = new Random();
-    String[] Adult= {};
-    System.out.println("Adult List:  " +  Adult;[generate.nextInt()];
-
-      // Generate random list for child
-    Random generate = new Random();
-    String[] Child = {};
-    System.out.println("Child List:  " +  Child;[generate.nextInt()];
-
-    // Create each of the local variables for the person's characteristics. - Written by Garhett
-
-    String name = "";
-    String gender = "";
-    int age = 0;
+    Scanner in = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
     // Create each local list to be used and assigned for various uses. - Written by Garhett
-    ArrayList<String> dislikedPeople = new ArrayList<>();
-    ArrayList<String> likedItems = new ArrayList<>();
-    ArrayList<Person> personList = new ArrayList<>();
+    ArrayList<Person> familyList = new ArrayList<>();
+
+    System.out.println("Welcome to Gift Exchange Builder!\n");
+    System.out.println("Family gift exchanges are a holiday tradition in many families.\nThe user will have the ability to generate a random report if they need to \ncreate a new gift exchange list for this year.\n");
+    System.out.print("How many family members would you like to add to a gift exchange report?: ");
+    final int FAMILY_MEMBER_COUNT = in.nextInt();
+    for (int i = 0; i < FAMILY_MEMBER_COUNT; i++) {
+      in.nextLine();
+      int y = i + 1;
+      System.out.println("\nFamily member #" + y + "\n_______________");
+      System.out.print("Enter a name: ");
+      String name = in.nextLine();
+      System.out.print("Enter " + name + "'s gender: ");
+      String gender = in.nextLine();
+      System.out.print("Enter " + name + "'s age: ");
+      int age = in.nextInt();
+      if (age < 18) {
+        System.out.print("Enter a present " + name + " wants for Christmas: ");
+        String presentTheyWant = scanner.nextLine();
+        familyList.add(new Child(name, gender, age, presentTheyWant));
+      } else {
+        System.out.print("Does " + name + " dislike anyone in the family? (yes or no): ");
+        String doTheyDislikeAnyone = in.nextLine();
+        String blacklistedPerson = "None";
+        if (doTheyDislikeAnyone.equalsIgnoreCase("Yes")) {
+          System.out.print(
+              "Enter name of disliked family member. (This person WILL NOT exchange gifts with "
+                  + name
+                  + ".: ");
+          blacklistedPerson = in.nextLine();
+        }
+        familyList.add(new Adult(name, gender, age, blacklistedPerson));
+      }
+    }
   }
 }
